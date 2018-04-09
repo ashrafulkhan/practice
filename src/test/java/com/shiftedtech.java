59a85e789826adfe6e1e2dad3defe1fbeb053d7a
ashrafulkhan <islam49968@yahoo.com>
2018-04-08 20:18:01 -0400

    first commit

---
+++ src/test/java/com/shiftedtech.java
@@ -0,0 +1,24 @@
+package com;
+
+public class shiftedtech {
+    public void test(){
+        System.out.println("test case is procedure ");
+
+
+    }
+
+    public void test01(){
+        System.out.println("second test for git ");
+    }
+
+    public void test02(){
+        System.out.println("third test for git");
+    }
+    public void test03(){
+        System.out.println("third test for git");
+    }
+
+    public void test04(){
+        System.out.println("fift commit for git ");
+    }
+}
